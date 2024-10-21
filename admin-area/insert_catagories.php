edad64cff42d787a9648cda67484f0f08e88c9b2
Abyalew <labyalew@gmail.com>
2024-10-21 10:09:07 -0700

    add some future

--- admin-area/insert_catagories.php
+++ admin-area/insert_catagories.php
@@ -1,3 +1,19 @@
+<?php 
+include("../includes/connect.php");
+
+if (isset($_POST['insert_cat'])) {
+    $catagory_title = mysqli_real_escape_string($con, $_POST['cat_title']);
+    $insert_query = "INSERT INTO `catagorie` (catagorie_title) VALUES ('$catagory_title')";
+    $result = mysqli_query($con, $insert_query);
+    
+    if ($result) {
+        echo "<script>alert('Category has been inserted successfully');</script>";
+    } else {
+        die("Query failed: " . mysqli_error($con));
+    }
+}
+?>
+
 <form action="" method="post" class="mb-2">
     <div class="input-group mb-2 w-90">
         <span class="input-group-text bg-success" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
@@ -6,8 +22,8 @@
     </div>
     <div class="input-group mb-2 w-10 m-auto">
 
-        <!-- <input type="submit" class="form-control bg-success" name="insert_cat" value="Insert Categories"> -->
+        <input type="submit" class=" bg-success border-0 p-2 my-3" name="insert_cat" value="Insert Categories">
 
-        <button class="bg-success my-3 p-2 border-0"> Insert Catagories</button>
+
     </div>
 </form>
